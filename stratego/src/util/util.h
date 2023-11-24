44320388b0a18fba52fb0401f8a9e2400f0911a3
mmb <mees@connectstuff.nl>
2023-11-24 11:38:44 +0100

    initial commit

---
+++ stratego/src/util/util.h
@@ -0,0 +1,12 @@
+//
+// Created by MB on 22/11/2023.
+//
+
+#ifndef STRATEGO_UTIL_H
+#define STRATEGO_UTIL_H
+
+#include "../board/board.h"
+
+void printBoard(struct Board* board);
+
+#endif //STRATEGO_UTIL_H
